4b4575c81b42f57fbb211d165850e5c46e0bae7a
Jaxon2020 <61923016+Jaxon2020@users.noreply.github.com>
2023-01-17 09:59:55 -0600

    Add files via upload

---
+++ enum.py
@@ -0,0 +1,24 @@
+import subprocess
+
+def main():
+
+    ART4 = """
+    
+    
+███████████████████████████████████
+█─▄▄▄─█─▄▄─█▄─█─▄█─▄▄─█─▄─▄─█▄─▄▄─█
+█─███▀█─██─██▄─▄██─██─███─████─▄█▀█
+▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀
+    
+    
+
+    """
+
+    print(ART4)
+
+    input("Enter IP or domain to enumerate(Currently only able to do one at a time): ")
+
+    
+
+if __name__ == "__main__":
+    main()
