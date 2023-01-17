3b45c13d5cd55c786b55f1a5410a26f2ee523b24
Jaxon2020 <61923016+Jaxon2020@users.noreply.github.com>
2023-01-17 19:33:00 +0000

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-echo "give me a bottle of rum!"
+echo "Intiating Web enumeration..."
 
 mapfile -t open_ports < open_ports.txt
 mapfile -t IPs < ip.txt
 
+IPmain=${IPs[0]}
 
 for port in "${open_ports[@]}"; do
     if [ $port == "80" ]; then
         echo "Port 80 is open, proceeding with enumeration..."
         echo "Utilizing tools to find and map domain if possible..."
 
-        domain=$(curl -s -i http://10.10.11.180:80 | grep "Location" | awk '{print $2}' | sed 's#http://##g')
+        domain=$(curl -s -i http://$IPmain:80 | grep "Location" | awk '{print $2}' | sed 's#http://##g')
 
-        echo "$IP $domain" | sudo tee -a /etc/hosts
-        echo $domain
+        echo "$IPmain $domain" | sudo tee -a /etc/hosts
+    
+        echo "Domain found and successfully mapped!"
+
+        echo "Looking for subdomains..."
+
+        ffuf -w /opt/useful/SecLists/Discovery/DNS/bitquark-subdomains-top100000.txt:FUZZ -u http://FUZZ.github.com:80/ 
+
+
     else
         echo "The port $port does not seem to be hosting a webservice.."
     fi
